985447118a43dde7d06ca1fbb0502af912af769b
aleks1 <aleksey.shkirko@gmail.com>
2015-11-05 18:48:50 +0500

    1

--- afishaWebApi/Models/DataClasses/MovieDetails.cs
+++ afishaWebApi/Models/DataClasses/MovieDetails.cs
@@ -9,14 +9,54 @@ namespace afishaWebApi.Models.DataClasses
         public MovieDetails data { get; set; }
     }
 
+    public class Show
+    {
+        public string cinema_id { get; set; }
+        public string cinema { get; set; }
+        public List<string> times { get; set; }
+        public List<string> formatted_times { get; set; }
+        public string format { get; set; }
+        public string doubled { get; set; }
+        public string first { get; set; }
+        public string lang { get; set; }
+        public string subtitle_lang { get; set; }
+    }
+
+    public class Afisha
+    {
+        public string show_date { get; set; }
+        public List<Show> shows { get; set; }
+    }
+
+    public class Comment
+    {
+        public string avatar { get; set; }
+        public string comment { get; set; }
+        public string comment_id { get; set; }
+        public string date_added { get; set; }
+        public string user_name { get; set; }
+    }
+
+    public class Photo
+    {
+        public string image_small_path { get; set; }
+        public string image_big_path { get; set; }
+    }
+
+    public class Review
+    {
+        public string material_id { get; set; }
+        public string intro { get; set; }
+    }
+
     public class MovieDetails
     {
         public string actors { get; set; }
-        public List<object> afisha { get; set; }
+        public List<Afisha> afisha { get; set; }
         public string alt_name { get; set; }
         public string avg_rate { get; set; }
         public string budget { get; set; }
-        public List<object> comments { get; set; }
+        public List<Comment> comments { get; set; }
         public string country { get; set; }
         public string date_premiere { get; set; }
         public string description { get; set; }
@@ -35,7 +75,7 @@ namespace afishaWebApi.Models.DataClasses
         public string name { get; set; }
         public string original { get; set; }
         public string permalink { get; set; }
-        public List<object> photos { get; set; }
+        public List<Photo> photos { get; set; }
         public string production { get; set; }
         public string rating { get; set; }
         public string rating_code { get; set; }
@@ -44,11 +84,5 @@ namespace afishaWebApi.Models.DataClasses
         public string trailer_moveruz { get; set; }
         public string url { get; set; }
         public string voices { get; set; }
-    }
-
-    public class Review
-    {
-        public string material_id { get; set; }
-        public string intro { get; set; }
     }
 }
